d6cbe023c655230abfc5c4e35fe05fc0f4a11d81
Ankit Yadav <ankityadav.webdev@gmail.com>
2024-07-10 01:11:23 +0530

    fix bug - add suspense boundary

--- src/app/(user)/(home)/products/layout.tsx
+++ src/app/(user)/(home)/products/layout.tsx
@@ -1,12 +1,16 @@
 import { Metadata } from 'next';
-import { ReactNode } from 'react';
+import { ReactNode, Suspense } from 'react';
 
 export const metadata: Metadata = {
     title: 'Our Store',
 };
 
 const RootLayout = ({ children }: { children: ReactNode }) => {
-    return <main>{children}</main>;
+    return (
+        <main>
+            <Suspense>{children}</Suspense>
+        </main>
+    );
 };
 
 export default RootLayout;
